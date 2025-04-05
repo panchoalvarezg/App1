95f54ac1eb08a945a04e0cbd4abfa9dc0cf22fac
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-04-04 22:34:29 -0300

    Update metrics.c

--- app1/src/metrics.c
+++ app1/src/metrics.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "metrics.h"
+#include "inlude/metrics.h"
 
 char* total_sales(int *size, struct order *orders) {
     float total = 0.0f;

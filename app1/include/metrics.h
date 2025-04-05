8aa0c94601217189a48e60d22257192f21490ad1
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-04-04 23:10:58 -0300

    Update metrics.h

--- app1/include/metrics.h
+++ app1/include/metrics.h
@@ -5,4 +5,16 @@
 typedef char* (*metric_func)(int *size, struct order *orders);
 metric_func get_metric(const char *name);
 
+// Declaraciones de las nuevas funciones
+char* pms(int *size, struct order *orders);
+char* pls(int *size, struct order *orders);
+char* dms(int *size, struct order *orders);
+char* dls(int *size, struct order *orders);
+char* dmsp(int *size, struct order *orders);
+char* dlsp(int *size, struct order *orders);
+char* apo(int *size, struct order *orders);
+char* apd(int *size, struct order *orders);
+char* ims(int *size, struct order *orders);
+char* hp(int *size, struct order *orders);
+
 #endif

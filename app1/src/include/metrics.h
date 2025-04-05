628c6b8f9c3bd00e9f691cb726ea11086f36a930
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-04-05 00:23:00 -0300

    Update metrics.h

--- app1/src/include/metrics.h
+++ app1/src/include/metrics.h
@@ -1,6 +1,6 @@
 #ifndef METRICS_H
 #define METRICS_H
-#include "src/parser.h"
+#include "parser.h"
 
 typedef char* (*metric_func)(int *size, struct order *orders);
 metric_func get_metric(const char *name);
